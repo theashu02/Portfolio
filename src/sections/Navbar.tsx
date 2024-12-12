167e87e1939cbcbeaea5cd5e4bbb58d4b82b0558
theashu02 <ashutoshchauhan1919@gmail.com>
2024-12-12 15:00:45 +0530

    update resume

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -140,7 +140,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+            <Button text="Resume" link="https://ashuchauhan.vercel.app/resume.pdf" />
           </motion.div>
         </div>
       </div>
